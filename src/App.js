3439c74952bd081187d0a615c62587733166f4d2
wiwi-git <duse34@hotmail.com>
2022-01-06 20:03:50 +0900

    upload - 7.2.3ing

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import User from "./components/User";
+import { UserProvider } from "./contexts/User";
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #ffffff;
+    justify-content: center;
+    align-items: center;   
+`;
+
+const App = () => {
+    return (
+        <UserProvider>
+            <Container>
+                <User />
+            </Container>
+        </UserProvider>
+    );
+};
+
+export default App;
